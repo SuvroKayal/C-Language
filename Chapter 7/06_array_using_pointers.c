31f764fcd9b2ab3020dfcb846e4629f6e0ce12fc
Suvro Kayal <suvrokayal2005@gmail.com>
2026-06-11 20:49:00 +0530

    Array is completed

--- Chapter 7/06_array_using_pointers.c
+++ Chapter 7/06_array_using_pointers.c
@@ -6,5 +6,12 @@ int main() {
     // int* ptr = &marks[0];
     int* ptr = marks; //Same as int* ptr = &marks[0];
 
+    for(int i = 0; i < 4; i++){
+        // printf("The marks at index %d is %d\n", i, marks[i]);
+        printf("The marks at index %d is %d\n", i, *ptr);
+        ptr++;
+    }
+    
+
     return 0;
 }
