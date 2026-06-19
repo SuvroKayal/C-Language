d3477a35ff587599f1fdb6ba3925575b7759517a
Suvro Kayal <suvrokayal2005@gmail.com>
2026-06-19 19:54:50 +0530

    Chapter 8 Practice set is completed

---
+++ Chapter 8/Practice Set/06_problem6.c
@@ -0,0 +1,18 @@
+/*
+Write a program to encrypt a string by adding 1 to the ascii value of its 
+characters. 
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[30];
+    scanf("%[^\n]", str);
+    for (int i = 0; i < strlen(str); i++)
+    {
+        str[i] = str[i] + 1;
+    }
+    
+    printf("%s", str);
+    return 0;
+}
---
+++ Chapter 8/Practice Set/09_problem9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char c = 's';
+    int contains = 0;
+    char str[] = "suvro";
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if(str[i] == c){
+         contains = 1;
+        }
+    }
+    if(contains){
+        printf("Yes it contains");
+    }
+    else{
+        printf("No it not contains");
+    }
+    return 0;
+}
